2a136017067088a43110d5d6c577cd335b978fe7
Christian Chastain <chastain_christian@hotmail.com>
2026-07-20 06:15:46 -0700

    Add partner application submission and status endpoints

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ from app.routers.organization_chats import router as organization_chats_router
 from app.routers import organization_chats
 from app.routers.partner_applications import router as partner_applications_router
 from app.routers import organization_chats
+from app.routers.partner_applications import router as partner_applications_router
 from app.config.redis import init_redis, close_redis, get_redis
 from app.routers.organizations_events import router as organizations_events_router
 from app.config.redis import init_redis, close_redis
