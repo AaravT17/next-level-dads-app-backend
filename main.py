f4eeb92a6e881786ef0fbefb15d1816eb76028e2
Christian Chastain <chastain_christian@hotmail.com>
2026-07-20 06:15:46 -0700

    Add partner application submission and status endpoints

--- main.py
+++ main.py
@@ -14,6 +14,7 @@ from app.routers.moderation import router as moderation_router
 from app.routers.admin import router as admin_router
 from app.routers.chats import router as chats_router
 from app.routers.ws import router as ws_router
+from app.routers.partner_applications import router as partner_applications_router
 from app.config.redis import init_redis, close_redis, get_redis
 from app.ws.pubsub import init_pubsub, close_pubsub
 from fastapi.middleware.cors import CORSMiddleware
@@ -23,6 +24,7 @@ from app.config.supabase import init_supabase
 import asyncpg
 from fastapi_limiter import FastAPILimiter
 from app.config.rate_limits import is_production
+
 
 
 @asynccontextmanager
@@ -63,6 +65,7 @@ app.add_middleware(
     allow_headers=['*'],
 )
 
+app.include_router(partner_applications_router)
 app.include_router(auth_router)
 app.include_router(users_router)
 app.include_router(interests_router)
